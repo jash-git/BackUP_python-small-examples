29941ff2ae089e273815bae1cef3e75748a398fa
Gitea <gitea@fake.local>
2020-06-05 09:42:05 +0800

    Add 202006050942

---
+++ WEB/HTML/python-small-examples.readthedocs.io/en/latest/_static/readthedocs-data.js
@@ -0,0 +1,19 @@
+var READTHEDOCS_DATA = {
+    project: "python-small-examples",
+    version: "latest",
+    language: "en",
+    programming_language: "words",
+    subprojects: {},
+    canonical_url: "https://python-small-examples.readthedocs.io/en/latest/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/rst/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "a27be638",
+    ad_free: false,
+
+    global_analytics_code: "UA-17997319-1",
+    user_analytics_code: null
+};
+
